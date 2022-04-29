e14b373b663563b7a406a2aabef22e1ded3ad083
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-29 00:48:11 +0000

    changed html

--- js/script.js
+++ js/script.js
@@ -14,16 +14,48 @@ if (navigator.serviceWorker) {
   })
 }
 
-/**
- * This function converts the temperature from fahrenheit to celsius.
- */
-function convert() {
-  // input
-  const fahrenheitTemperature = parseFloat(document.getElementById('fahrenheit-temperature').value)
+const priceSmall = 4.00;
+const priceMedium = 5.00;
+const priceLarge = 7.00;
+const priceToppings = 0.50; 
+const pst = .07;
+const gst = .07
+let subtotal = 0;
 
-   // process
-  const temperature = (fahrenheitTemperature - 32) * 5 / 9
+function calculateSize(element) {
+  if (element == null) {
+    return;
+  }
 
-  // output
-  document.getElementById('temperature').innerHTML = '<p>The temperature in Celsius is: ' + temperature.toFixed(2) + ' °C</p>'
+  switch(element.value) {
+    case "10":
+      subtotal = priceSmall;
+      break;
+    case "12":
+      subtotal = priceMedium;
+      break;
+    case "15":
+      subtotal = priceLarge;
+      break;
+  }
+    recalculateTotal();
+}
+
+function toppingOptionPrice(element) {
+  if (element.checked) {
+      subtotal += priceToppings;
+  } else {
+      subtotal -= priceToppings;
+  }
+    recalculateTotal();
+}
+
+function recalculateTotal() {
+  let pstToPay = subtotal * pst;
+  let gstToPay = subtotal * gst;
+  let total = subtotal + pstToPay + gstToPay;
+  document.getElementById("subtotal").value = subtotal;
+  document.getElementById("pstToPay").value = pstToPay;
+  document.getElementById("gstToPay").value = gstToPay;
+  document.getElementById("total").value = total;
 }
